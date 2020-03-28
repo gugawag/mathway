821d00ad8d437a5b9721e5d54f9afe48a2ae21c0
Taw-ham <taw-ham@MacBook-Air-de-Taw-ham.local>
2020-03-28 10:42:33 -0300

    teste 3 todos os codigos

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # mathway
-# mathway
---
+++ comentario.js
@@ -0,0 +1,14 @@
+let comentario = document.getElementById("enviar");
+let escrever = document.getElementById("comentario");
+let comentarios = [];
+let posicao = 0
+comentario.onclick = function(){
+    if (escrever.value != " ") {
+        comentarios.push(escrever.value);
+        let li = document.createElement("li");
+        li.innerHTML = comentarios[posicao];
+        document.getElementById("area_comentario").append(li);
+        posicao++;
+        escrever.value = " ";
+    }
+}
